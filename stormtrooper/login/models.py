e16166d6c2d1aec451ee89966453b4864f415c80
Sebin Thomas <sebin.t@compile.com>
2016-06-06 16:03:22 +0530

    Adding custom user model

--- stormtrooper/login/models.py
+++ stormtrooper/login/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model
+    """
+
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
--- stormtrooper/stormtrooper/local_settings.py
+++ stormtrooper/stormtrooper/local_settings.py
@@ -4,3 +4,5 @@ MEDIA_ROOT = "%s/media/" % (BASE_DIR)
 STATIC_ROOT = "%s/static/" % (BASE_DIR)
 
 MEDIA_URL = '/media/'
+
+AUTH_USER_MODEL = 'login.CustomUser'
