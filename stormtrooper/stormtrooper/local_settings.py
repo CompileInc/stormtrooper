abb176ca9faf86520b2cf9fe1bb252160fddc9d4
Sebin Thomas <sebin.t@compile.com>
2016-06-06 19:08:16 +0530

    Adding google API key

--- stormtrooper/stormtrooper/local_settings.py
+++ stormtrooper/stormtrooper/local_settings.py
@@ -4,3 +4,6 @@ MEDIA_ROOT = "%s/media/" % (BASE_DIR)
 STATIC_ROOT = "%s/static/" % (BASE_DIR)
 
 MEDIA_URL = '/media/'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '326290971528-4174mkl76s9rpm5260cs5qb4jfjjtfom.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'iIzzqq7MCZ7WAayzagO2ekyy'
