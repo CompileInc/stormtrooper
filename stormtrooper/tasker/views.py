287162d7b5ffdf9ff06ee388409dc5ec608a811d
Sebin Thomas <sebin.t@compile.com>
2016-06-07 16:01:52 +0530

    Closed tasks cannot be played and fix for issue with showing closed tag

--- stormtrooper/tasker/views.py
+++ stormtrooper/tasker/views.py
@@ -3,13 +3,14 @@ from tasker.models import Task, Question
 from django.views.generic.detail import DetailView
 from django.views.generic import View
 from django.http.response import Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 class TaskListView(ListView):
     model = Task
 
     def get_queryset(self):
-        if self.model.created_by is self.request.user:
+        if self.request.user.is_superuser:
             return self.model.objects.all_active()
         return self.model.objects.active()
 
@@ -23,8 +24,10 @@ class TaskPlayView(View):
     def get(self, request, pk):
         try:
             task_obj = Task.objects.get(pk=pk)
-            random_qn = task_obj.random_question(user=request.user)
-            return HttpResponseRedirect(random_qn.get_absolute_url())
+            if not task_obj.is_closed:
+                random_qn = task_obj.random_question(user=request.user)
+                return HttpResponseRedirect(random_qn.get_absolute_url())
+            return HttpResponseRedirect(reverse('task-list'))
         except Task.DoesNotExist:
             return Http404
 
