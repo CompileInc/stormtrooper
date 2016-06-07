d02fab8c5c72cc6c5ae0062fe86d9226fe2144fd
Sebin Thomas <sebin.t@compile.com>
2016-06-07 12:29:43 +0530

    Displaying a random unanswered question when users click play

--- stormtrooper/tasker/views.py
+++ stormtrooper/tasker/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.list import ListView
-from tasker.models import Task
+from tasker.models import Task, Question
 from django.views.generic.detail import DetailView
-
+from django.views.generic import View
+from django.http.response import Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 class TaskListView(ListView):
     model = Task
@@ -14,3 +16,18 @@ class TaskListView(ListView):
 
 class TaskDetailView(DetailView):
     model = Task
+
+
+class TaskPlayView(View):
+
+    def get(self, request, pk):
+        try:
+            task_obj = Task.objects.get(pk=pk)
+            random_qn = task_obj.random_question(user=request.user)
+            return HttpResponseRedirect(reverse('question-detail', kwargs={'slug': random_qn.slug}))
+        except Task.DoesNotExist:
+            return Http404
+
+
+class QuestionDetailView(DetailView):
+    model = Question
