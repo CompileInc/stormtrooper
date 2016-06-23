7feaf558914bbe22d28b9129e7c82cc9587e5b7b
Cyriac Thomas <cyriacsmail@gmail.com>
2016-06-23 11:47:49 +0530

    fixed profile url to accept all possible values

--- stormtrooper/login/urls.py
+++ stormtrooper/login/urls.py
@@ -6,6 +6,6 @@ from login.views import LoginView, TrooperView
 
 urlpatterns = (url(r'^login/$', LoginView.as_view(), name="accounts-login"),
                url(r'^logout/$', logout, {'template_name': 'login/logout.html'}, name="accounts-logout"),
-               url(r'^u/(?P<username>[\w-]+)/$', TrooperView.as_view(), name="accounts-profile"),
+               url(r'^u/(?P<username>[(\w)(\W)(\s)]+)/$', TrooperView.as_view(), name="accounts-profile"),
                url('', include('social.apps.django_app.urls', namespace='social')),
                )
