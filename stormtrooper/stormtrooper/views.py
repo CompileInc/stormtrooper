2dee9be3fdc48a1a46c73e6d342f1866adff0238
Cyriac Thomas <cyriacsmail@gmail.com>
2016-06-29 10:47:49 +0530

    new homepage

--- stormtrooper/stormtrooper/views.py
+++ stormtrooper/stormtrooper/views.py
@@ -1,9 +1,11 @@
 from django.http.response import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 
 def home(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('task-list'))
     else:
-        return HttpResponseRedirect(reverse('accounts-login'))
+        return render_to_response('stormtrooper/homepage.html', RequestContext(request, {}))
