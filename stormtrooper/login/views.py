9c4face3e3dc4f22b6e1e9ad07eedc38cb0f2fd7
Cyriac Thomas <cyriacsmail@gmail.com>
2016-07-01 15:23:51 +0530

    adding login for profile page

--- stormtrooper/login/views.py
+++ stormtrooper/login/views.py
@@ -2,6 +2,8 @@ from django.core.urlresolvers import reverse
 from django.http.response import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from login.models import Trooper
 
@@ -15,6 +17,7 @@ class LoginView(TemplateView):
         return super(LoginView, self).dispatch(request, *args, **kwargs)
 
 
+@method_decorator(login_required, name='dispatch')
 class TrooperView(DetailView):
     slug_field = 'username'
     slug_url_kwarg = 'username'
