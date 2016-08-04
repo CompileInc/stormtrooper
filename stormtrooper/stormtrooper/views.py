e6b887e1a4f0b832f9c83415cfd7a58cf99115e3
Cyriac Thomas <cyriacsmail@gmail.com>
2016-08-04 18:04:01 +0530

    updating to django 1.10 and some perf improvements

--- stormtrooper/stormtrooper/views.py
+++ stormtrooper/stormtrooper/views.py
@@ -1,11 +1,10 @@
 from django.http.response import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
+from django.shortcuts import render
 
 
 def home(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('task-list'))
     else:
-        return render_to_response('stormtrooper/homepage.html', RequestContext(request, {}))
+        return render(request, 'stormtrooper/homepage.html')
