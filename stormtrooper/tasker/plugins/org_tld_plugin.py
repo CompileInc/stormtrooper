75da13c793c506921a68ac8dcccb9b097530f712
Cyriac Thomas <cyriacsmail@gmail.com>
2016-08-09 11:21:41 +0530

    plugin to normalize websites with subdomains

--- stormtrooper/tasker/plugins/org_tld_plugin.py
+++ stormtrooper/tasker/plugins/org_tld_plugin.py
@@ -8,7 +8,7 @@ class WebsiteTLD(Plugin):
     plugin_name_verbose = "Website TLD"
 
     @classmethod
-    def process(self, answers):
+    def process(cls, answers):
         import tldextract
         _answers = []
         for answer in answers:
@@ -18,4 +18,31 @@ class WebsiteTLD(Plugin):
             except Exception as e:
                 domain = ''
             _answers.append(domain)
+        return _answers
+
+
+class WebsiteTLDSub(Plugin):
+    plugin_name = "wtlds"
+    plugin_name_verbose = "Website TLD (with subdomain)"
+
+    @classmethod
+    def process(cls, answers):
+        import re
+        import tldextract
+        from django.core.validators import EMPTY_VALUES
+        _answers = []
+        for answer in answers:
+            extract = tldextract.extract(answer)
+            if extract.domain in EMPTY_VALUES or\
+               extract.suffix in EMPTY_VALUES:
+                tmp_domain = ''
+            else:
+                extract = list(extract)
+                tmp_domain = ".".join(extract)
+                p = re.compile("^[w]{1,3}([\d]{0,2})?\.")
+                m = p.search(tmp_domain)
+                if m:
+                    tmp_domain = tmp_domain[len(m.group(0)):]
+
+            _answers.append(tmp_domain.lstrip(".").lower())
         return _answers
