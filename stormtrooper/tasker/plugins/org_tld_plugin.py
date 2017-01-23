e79cb50bb7883c2027c72858b26841baa748e1cc
Sebin Thomas <sebin.t@compile.com>
2017-01-23 13:34:00 +0530

    Add new plugin to do fuzzy matching on websites

--- stormtrooper/tasker/plugins/org_tld_plugin.py
+++ stormtrooper/tasker/plugins/org_tld_plugin.py
@@ -46,3 +46,85 @@ class WebsiteTLDSub(Plugin):
 
             _answers.append(tmp_domain.lstrip(".").lower())
         return _answers
+
+
+class FuzzyWebsiteMatch(Plugin):
+    plugin_name = 'fzweb'
+    plugin_name_verbose = 'Fuzzy Website match (with subdomain)'
+    COMPUTE_ANSWER = True
+
+    @staticmethod
+    def get_majority_item(items):
+        from collections import Counter
+        c = Counter(items)
+        most_common = c.most_common()
+        max_freq = most_common[0][1]
+        if max_freq == 1:
+            return None
+        if len(c) > 1 and not max_freq > most_common[1][1]:
+            return None
+        else:
+            return most_common[0][0]
+
+    @staticmethod
+    def get_website_tld(domain):
+        import re
+        import tldextract
+        from django.core.validators import EMPTY_VALUES
+        domain = domain.strip().lower()
+        extract = tldextract.extract(domain)
+        if extract.domain in EMPTY_VALUES or\
+           extract.suffix in EMPTY_VALUES:
+            tmp_domain = ''
+        else:
+            extract = list(extract)
+            tmp_domain = ".".join(extract)
+            p = re.compile("^[w]{1,3}([\d]{0,2})?\.")
+            m = p.search(tmp_domain)
+            if m:
+                tmp_domain = tmp_domain[len(m.group(0)):]
+        return tmp_domain.lstrip(".").strip()
+
+    @classmethod
+    def normalize_website(cls, w):
+        from django.core.validators import EMPTY_VALUES
+        from urlparse import urlparse, urlunparse, ParseResult
+        if w in EMPTY_VALUES:
+            return None
+        w = w.lower().strip()
+        if not w.startswith('http://') and not w.startswith('https://'):
+            w = 'http://' + w.lstrip('/')
+        parsed = urlparse(w)
+        new_parsed = ParseResult(scheme='http',
+                                 netloc=cls.get_website_tld(w),
+                                 path=parsed.path.rstrip('/'),
+                                 params='',
+                                 query=parsed.query,
+                                 fragment='')
+        return urlunparse(new_parsed)
+
+    @classmethod
+    def process(cls, answers):
+        from collections import defaultdict
+        websites = map(cls.normalize_website, answers)
+        result_website = None
+        votes = 0
+        website_tlds = defaultdict(list)
+        _website_tlds = []
+        for w in websites:
+            website_tld = cls.get_website_tld(w)
+            _website_tlds.append(website_tld)
+            website_tlds[website_tld].append(w)
+        common_website_tld = cls.get_majority_item(_website_tlds)
+        if common_website_tld is None:
+            return (None, 0)
+        common_websites = website_tlds[common_website_tld]
+        common_website = cls.get_majority_item(common_websites)
+        if common_website:
+                result_website = common_website
+        else:
+            result_website = cls.normalize_website(cls.get_website_tld(common_websites[0]))
+        for website in websites:
+            if result_website in website:
+                votes += 1
+        return (result_website, votes)
